6726f68d71b47a8a98ff334f04303928f2a91afe
Klement <anselmo.klement.chua@gmail.com>
2024-11-25 18:54:30 +0800

    Added ability to add todos to projects

--- src/index.js
+++ src/index.js
@@ -19,22 +19,22 @@ import './styles.css';
 function createProject(title) {
   let notes = [];
 
-  const getNotes = () => notes;
+  const getCards = () => notes;
 
-  const addNote = function (note) {
+  const addCard = function (note) {
     notes.push(note);
   };
 
-  const delNote = function (title) {
+  const delCard = function (title) {
     notes = notes.filter((note) => note.note !== title);
   };
 
-  return { title, getNotes, addNote, delNote };
+  return { title, getCards, addCard, delCard };
 }
 
-// Note constructor -------------
-function createNote(note) {
-  return { note };
+// Card constructor -------------
+function createCard(title, description, dueDate, priority) {
+  return { title, description, dueDate, priority };
 }
 
 // Controller -------------
@@ -78,32 +78,54 @@ const DOMHandler = (function handleMainDOMFunctions() {
       }
     }
 
-    const noteContainer = document.querySelector('.note-container');
-    noteContainer.innerHTML = '';
-    for (const note of Controller.getCurrProject().getNotes()) {
-      noteContainer.innerHTML += `<div class="note">${note.note}</div>`;
+    const cardContainer = document.querySelector('.card-container');
+    cardContainer.innerHTML = '';
+    for (const card of Controller.getCurrProject().getCards()) {
+      cardContainer.innerHTML += `
+        <div class="todo-card">
+            <div class="priority-block ${card.priority}-priority"></div>
+            <div class="info-block">
+                <h2>${card.title}</h2>
+                <p>${card.description}</p>
+                <p><strong>Due: </strong>${card.dueDate}</p>
+            </div>
+            <div class="delete-block">
+                <svg id="trash-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>
+            </div>
+            <div class="complete-block">
+                <button class="complete-toggle incomplete">In Progress</button>
+            </div>
+        </div>`;
     }
   }
 
   return { loadPage };
 })();
 
-const NoteHandler = (function handleNoteDOM() {
-  function handleInput(input, event) {
-    const currProject = Controller.getCurrProject();
-    switch (event.key) {
-      case 'Enter':
-        currProject.addNote(createNote(event.target.value));
-        DOMHandler.loadPage();
-        input.value = '';
-        break;
-      case 'Escape':
-        input.value = '';
-        input.blur();
-        break;
-    }
+const CardHandler = (function handleCardDOM() {
+  function showModal() {
+    const newCardDialog = document.querySelector('#new-todo-dialog');
+    newCardDialog.showModal();
   }
-  return { handleInput };
+
+  function handleInput(event) {
+    event.preventDefault();
+    // Create to-do card
+    const title = document.querySelector('#note-title').value;
+    const description = document.querySelector('#note-description').value;
+    const dueDate = document.querySelector('#note-due-date').value;
+    const importance = document.querySelector('#note-importance').value;
+    const card = createCard(title, description, dueDate, importance);
+    // Add card to project, load to DOM and reset form
+    const currProject = Controller.getCurrProject();
+    currProject.addCard(card);
+    DOMHandler.loadPage();
+    const newToDoForm = document.querySelector('#new-todo-dialog form');
+    newToDoForm.reset();
+    const newToDoDialog = document.querySelector('#new-todo-dialog');
+    newToDoDialog.close();
+  }
+  return { showModal, handleInput };
 })();
 
 const ProjectHandler = (function handleProjectDOM() {
@@ -148,11 +170,13 @@ addProjectInput.addEventListener('keydown', (e) =>
   ProjectHandler.handleInput(addProjectInput, e)
 );
 
-const addNoteInput = document.querySelector('#add-note input');
-addNoteInput.addEventListener('keydown', (e) =>
-  NoteHandler.handleInput(addNoteInput, e)
-);
+// Add Todo Card
+const addCardBtn = document.querySelector('#add-card-btn');
+addCardBtn.addEventListener('click', CardHandler.showModal);
+const cardConfirmBtn = document.querySelector('#confirm-button');
+cardConfirmBtn.addEventListener('click', (e) => CardHandler.handleInput(e));
 
+// Switch projects
 const projectContainer = document.querySelector('.project-container');
 projectContainer.addEventListener('click', (e) =>
   ProjectHandler.handleSwitch(e.target)
